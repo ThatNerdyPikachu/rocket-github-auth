b226c632ec8730484a0153c25768a5d0eae4fd8b
Pika <pika@thom.guide>
2018-12-15 14:21:23 -0500

    oh no

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,45 @@ struct AccessTokenResponse {
     access_token: String,
 }
 
+#[derive(Deserialize, Serialize)]
+struct UserInfoResponse {
+    login: Option<String>,
+    name: Option<String>,
+    company: Option<String>,
+    location: Option<String>,
+    bio: Option<String>,
+    public_repos: Option<i32>,
+    followers: Option<i32>,
+    following: Option<i32>,
+}
+
+#[derive(Clone, Deserialize)]
+struct UserEmail {
+    email: String,
+    primary: bool,
+}
+
 #[derive(Serialize)]
 struct IndexViewContext {
     signed_in: bool,
+}
+
+#[derive(Serialize)]
+struct RedirectViewContext {
+    url: String,
+}
+
+#[derive(Serialize)]
+struct UserViewContext {
+    login: String,
+    name: String,
+    company: String,
+    location: String,
+    email: String,
+    bio: String,
+    public_repos: String,
+    followers: String,
+    following: String,
 }
 
 #[get("/")]
@@ -32,46 +68,42 @@ fn index(mut cookies: Cookies) -> Template {
     )
 }
 
-#[get("/login")]
-fn login(mut cookies: Cookies) -> Redirect {
+#[get("/login?<redirect_to>")]
+fn login(mut cookies: Cookies, redirect_to: Option<String>) -> Redirect {
     if cookies.get_private("state").is_some() {
         cookies.remove_private(Cookie::named("state"));
     }
 
-    println!("Generating state...");
+    if cookies.get_private("redirect_to").is_some() {
+        cookies.remove_private(Cookie::named("redirect_to"));
+    }
+
+    if redirect_to.is_some() {
+        cookies.add_private(Cookie::new("redirect_to", redirect_to.unwrap()));
+    }
 
     let state: String = thread_rng().sample_iter(&Alphanumeric).take(15).collect();
 
-    println!("Saving state...");
-
     cookies.add_private(Cookie::new("state", state.clone()));
 
-    println!("State saved!");
-
-    println!("Redirecting to GitHub...");
-
     Redirect::to(format!(
-        "https://github.com/login/oauth/authorize?client_id={}&state={}",
+        "https://github.com/login/oauth/authorize?client_id={}&scopes=user&state={}",
         env::var("CLIENT_ID").unwrap(),
         state
     ))
 }
 
 #[get("/callback?<code>&<state>")]
-fn callback(mut cookies: Cookies, code: String, state: String) -> String {
+fn callback(mut cookies: Cookies, code: String, state: String) -> Redirect {
     if state == cookies.get_private("state").unwrap().value() {
         cookies.remove_private(Cookie::named("state"));
-
-        println!("Code received: {}", code);
-
-        println!("Generating token...");
 
         let mut params = HashMap::new();
 
         params.insert("client_id", env::var("CLIENT_ID").unwrap());
         params.insert("client_secret", env::var("CLIENT_SECRET").unwrap());
         params.insert("code", code);
-        params.insert("state", state.clone());
+        params.insert("state", state);
 
         let resp: AccessTokenResponse = reqwest::Client::new()
             .post("https://github.com/login/oauth/access_token")
@@ -82,19 +114,19 @@ fn callback(mut cookies: Cookies, code: String, state: String) -> String {
             .json()
             .unwrap();
 
-        println!("Token received: {}", resp.access_token);
-
-        println!("Storing token...");
-
         cookies.add_private(Cookie::new("token", resp.access_token));
 
-        println!("Token stored!");
+        if cookies.get_private("redirect_to").is_some() {
+            let v = String::from(cookies.get_private("redirect_to").unwrap().value());
 
-        format!("Sweet! Your state ({}) matches the state that GitHub sent! You have been authenticated!", state)
+            cookies.remove_private(Cookie::named("redirect_to"));
+
+            Redirect::to(v)
+        } else {
+            Redirect::to("/")
+        }
     } else {
-        cookies.remove_private(Cookie::named("state"));
-        println!("Code received: {}", code);
-        format!("Uh-oh... Your state ({}) does not match the state that GitHub sent! Maybe try logging in again..?", state)
+        Redirect::to("/login")
     }
 }
 
@@ -103,9 +135,89 @@ fn logout(mut cookies: Cookies) -> Redirect {
     if cookies.get_private("token").is_some() {
         cookies.remove_private(Cookie::named("token"));
 
-        Redirect::to(uri!(index))
+        Redirect::to("/")
     } else {
-        Redirect::to(uri!(login))
+        Redirect::to("/login")
+    }
+}
+
+fn option_to_string(opt: &Option<String>) -> String {
+    if opt.is_some() {
+        opt.clone().unwrap()
+    } else {
+        String::from("None found!")
+    }
+}
+
+fn option_i32_to_string(opt: Option<i32>) -> String {
+    if opt.is_some() {
+        opt.unwrap().to_string()
+    } else {
+        String::from("None found!")
+    }
+}
+
+fn get_personal_email(emails: &[UserEmail]) -> UserEmail {
+    for e in emails.iter() {
+        if e.primary
+            || e.email.as_str().ends_with("gmail.com")
+            || e.email.as_str().ends_with("icloud.com")
+            || e.email.as_str().ends_with("hotmail.com")
+            || e.email.as_str().ends_with("yahoo.com")
+        {
+            return e.clone();
+        }
+    }
+
+    emails[0].clone()
+}
+
+#[get("/user")]
+fn user(mut cookies: Cookies) -> Template {
+    if cookies.get_private("token").is_none() {
+        Template::render(
+            "redirect",
+            RedirectViewContext {
+                url: String::from("/login?redirect_to=/user"),
+            },
+        )
+    } else {
+        let profile_resp: UserInfoResponse = reqwest::Client::new()
+            .get("https://api.github.com/user")
+            .header(
+                "Authorization",
+                format!("token {}", cookies.get_private("token").unwrap().value()),
+            )
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+
+        let emails_resp: Vec<UserEmail> = reqwest::Client::new()
+            .get("https://api.github.com/user/emails")
+            .header(
+                "Authorization",
+                format!("token {}", cookies.get_private("token").unwrap().value()),
+            )
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+
+        Template::render(
+            "user",
+            UserViewContext {
+                login: option_to_string(&profile_resp.login),
+                name: option_to_string(&profile_resp.name),
+                company: option_to_string(&profile_resp.company),
+                location: option_to_string(&profile_resp.location),
+                email: get_personal_email(&emails_resp).email,
+                bio: option_to_string(&profile_resp.bio),
+                public_repos: option_i32_to_string(profile_resp.public_repos),
+                followers: option_i32_to_string(profile_resp.followers),
+                following: option_i32_to_string(profile_resp.following),
+            },
+        )
     }
 }
 
@@ -113,6 +225,8 @@ fn main() {
     dotenv().ok();
     rocket::ignite()
         .attach(Template::fairing())
-        .mount("/", routes![index, login, callback, logout])
+        .mount("/", routes![index, login, callback, logout, user])
         .launch();
+
+    println!("nice");
 }
